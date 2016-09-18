0250e2ad9625ecc988d49e0ecfa67874ecf198ea
Joeran Vinzens <git@vinzens.me>
2016-09-18 12:26:58 +0200

    * Added funktionallity that if LED is not on, after time LED are set off again * Added funktionallity if LED are on, but nothing has changed, no command is send to LED strip * Deleted LCD Display

--- Arduino_ws2812_poti.ino
+++ Arduino_ws2812_poti.ino
@@ -1,13 +1,10 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <Adafruit_NeoPixel.h>
 #include <PinChangeInt.h>
 
 #define PIN 7
-#define NUMPIXELS 64
+#define NUMPIXELS 200
 #define BUTTON 8
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
 const int HueNumReadings = 10;
@@ -15,6 +12,8 @@ int HueReadings[HueNumReadings];
 int HueReadIndex = 0;
 int HueTotal = 0;
 int HueAverage = 0;
+int OldHueAverage = 0;
+int OldHueAverageDiviation = 0;
 int HueInputPin = A0;
 
 const int LightNumReadings = 10;
@@ -22,43 +21,32 @@ int LightReadings[LightNumReadings];
 int LightReadIndex = 0;
 int LightTotal = 0;
 int LightAverage = 0;
+int OldLightAverage = 0;
+int OldLightAverageDiviation = 0;
 int LightInputPin = A1;
 
 int RGBArray[3];
 
 int minLight = 0;
 int maxLight = 255;
+int idleRun = 0;
 
 boolean run = false;
-
+boolean buttonPressed = false;
 
 String rgbValue = "FFFFFF";
 
-
 void setup() {
   Serial.begin(9600);
-  lcd.begin(16,2);
-  for(int i = 0; i< 3; i++)
-  {
-    lcd.backlight();
-    delay(250);
-    lcd.noBacklight();
-    delay(250);
-  }
-  lcd.backlight();
-
   Serial.println("Hello!");
-  lcd.setCursor(0,0); //Start at character 4 on line 0
-  lcd.print("Hello, world!");
-  delay(1000);
   for (int thisReading = 0; thisReading < HueNumReadings; thisReading++) {
     HueReadings[thisReading] = 0;
   }
   for (int thisReading = 0; thisReading < LightNumReadings; thisReading++) {
     LightReadings[thisReading] = 0;
   }
-
   pixels.begin();
+
   PCintPort::attachInterrupt(BUTTON, buttonPress,CHANGE);
   for(int i=0;i<NUMPIXELS;i++) {
     pixels.setPixelColor(i,0,0,0);
@@ -76,7 +64,12 @@ void loop() {
     if (HueReadIndex >= HueNumReadings) {
       HueReadIndex = 0;
     }
+    OldHueAverage = HueAverage;
     HueAverage = HueTotal / HueNumReadings;
+    OldHueAverageDiviation=OldHueAverage-HueAverage;
+    if (OldHueAverageDiviation<0){
+      OldHueAverageDiviation=OldHueAverageDiviation*-1;
+    }
 
     LightTotal = LightTotal - LightReadings[LightReadIndex];
     LightReadings[LightReadIndex] = analogRead(LightInputPin);
@@ -85,37 +78,42 @@ void loop() {
     if (LightReadIndex >= LightNumReadings) {
       LightReadIndex = 0;
     }
+    OldLightAverage = LightAverage;
     LightAverage = LightTotal / LightNumReadings;
+    OldLightAverageDiviation = OldLightAverage-LightAverage;
+    if (OldLightAverageDiviation<0){
+      OldLightAverageDiviation=OldLightAverageDiviation*-1;
+    }
 
     getMinMaxLight(LightAverage);
-
-    getRGB(HueAverage);  
-    lcd.clear();
-    lcd.setCursor(0,0);
-    lcd.print("max:");
-    lcd.print(maxLight);
-    lcd.setCursor(9,0);
-    lcd.print("min:");
-    lcd.print(minLight);
-
-    lcd.setCursor(0,1);
-    lcd.print("r");
-    lcd.print(RGBArray[0]);
-    lcd.setCursor(5,1);
-    lcd.print("g");
-    lcd.print(RGBArray[1]);
-    lcd.setCursor(10,1);
-    lcd.print("b");
-    lcd.print(RGBArray[2]);
-    for(int i=0;i<NUMPIXELS;i++) {
-      pixels.setPixelColor(i, pixels.Color(RGBArray[0],RGBArray[1],RGBArray[2]));
+    getRGB(HueAverage);
+    if (OldLightAverageDiviation>4 or OldHueAverageDiviation>4 or idleRun>100) {
+      for(int i=0;i<NUMPIXELS;i++) {
+        pixels.setPixelColor(i, pixels.Color(RGBArray[0],RGBArray[1],RGBArray[2]));
+      }
+      Serial.println("Changing Pixels.......");
+      pixels.show();
+      idleRun=0;
+    } 
+    else {
+      Serial.println("Doing Nothing.. sleeping longer");
+      delay(100);
     }
-    pixels.show();
+    idleRun++;
     delay(100);
   }
   else
   {
     delay(500);
+    idleRun++;
+    if (idleRun > 1000) { // set LED to off from time to time because of failure in the LED's
+      idleRun=0;
+      for(int i=0;i<NUMPIXELS;i++) {
+        pixels.setPixelColor(i,0,0,0); // all off
+      }
+      pixels.show();
+    }
+
   }
 }
 
@@ -123,12 +121,12 @@ void getMinMaxLight(int light) {
   maxLight = 255;
   minLight = 0;
   switch(light) {
-  case 0 ... 512: 
+  case 0 ... 512:
     {
       maxLight = (light/512.0*255.0);
     }
     break;
-  case 513 ... 1023: 
+  case 513 ... 1023:
     {
       minLight = (light-512)/512.0*255.0;
     }
@@ -158,39 +156,39 @@ void getRGB(int hue) {
   int hueAngle = hue/1023.0*360.0;
   RGBArray[0] = minLight;
   RGBArray[1] = minLight;
-  RGBArray[2] = minLight; 
+  RGBArray[2] = minLight;
   switch(hueAngle) {
-  case 0 ... 60: 
+  case 0 ... 60:
     {
       RGBArray[0]=maxLight;
       RGBArray[1]=getColorValueRising(hueAngle, 0);
     }
     break;
-  case 61 ... 120: 
+  case 61 ... 120:
     {
       RGBArray[0]=getColorValueFalling(hueAngle, 60);
       RGBArray[1]=maxLight;
     }
     break;
-  case 121 ... 180: 
+  case 121 ... 180:
     {
       RGBArray[1]=maxLight;
       RGBArray[2]=getColorValueRising(hueAngle, 120);
     }
     break;
-  case 181 ... 240: 
+  case 181 ... 240:
     {
       RGBArray[1]=getColorValueFalling(hueAngle, 180);
       RGBArray[2]=maxLight;
     }
     break;
-  case 241 ... 300: 
+  case 241 ... 300:
     {
       RGBArray[0]=getColorValueRising(hueAngle, 240);
       RGBArray[2]=maxLight;
     }
     break;
-  case 301 ... 360: 
+  case 301 ... 355: // last 5° for white only. Here min value is set.
     {
       RGBArray[0]=maxLight;
       RGBArray[2]=getColorValueFalling(hueAngle, 300);
@@ -201,20 +199,31 @@ void getRGB(int hue) {
 }
 
 void buttonPress() {
-  if(digitalRead(BUTTON) == HIGH) {
-    if (run) {
-      for(int i=0;i<NUMPIXELS;i++) {
-        pixels.setPixelColor(i,0,0,0);
+  if (not buttonPressed) {
+    buttonPressed=true; // avoid double pressing the buttun while doing stuff
+    if(digitalRead(BUTTON) == HIGH) {
+      if (run) {
+        run=false;
+        for(int i=0;i<NUMPIXELS;i++) {
+          pixels.setPixelColor(i,0,0,0);
+        }
+        pixels.show();
+        delay(1000);
+        for(int i=0;i<NUMPIXELS;i++) {
+          pixels.setPixelColor(i,0,0,0);
+        }
+        pixels.show();
+
       }
-      pixels.show();
-      run=false;
-      delay(1000);
-    } 
-    else {
-      run=true;
-      delay(1000);
+      else {
+        run=true;
+        delay(1000);
+      }
     }
+    buttonPressed=false;
   }
 }
+
+
 
 
